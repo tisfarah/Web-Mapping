ff89c838e9c0b92f1fa3871fca2339e65cd554a8
Farah <mijameskac@gmail.com>
2019-06-26 23:51:23 -0400

    updates

---
+++ Unsolved 6/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiOGRlZWFrZXciLCJhIjoiY2p3dmEwanFpMDM4YzN5bnh4bzNzdW02ZiJ9.2MUZXPNvzW5IJQjbwL2AUw";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,9 @@
 // Creating map object
 var myMap = L.map("map", {
   center: [37.7749, -100.4194],
-  zoom: 5
+  zoom: 5,
+  // layers: [grayscale, cities]
+
 });
 
 // Adding tile layer
@@ -12,30 +14,26 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
+
 // Link to GeoJSON
 var APILink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-var markers = L.markerClusterGroup();
 
 console.log(APILink);
 
 
-
 d3.json(APILink, function(response) {
-  
+
+  console.log(response);
+
   for (var i = 0; i < response.length; i++) {
-    if ("latitude" in response[i] && "longitude" in response[i]) {
-      // From sample code --- markers.addLayer(L.marker(getRandomLatLng(map)));
-      markers.addLayer(L.marker([Number(response[i].latitude), Number(response[i].longitude)]));
+    var location = response[i].location;
+
+    if (location) {
+      L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
     }
+
   }
-  //From sample code - map.addLayer(markers); --- here map is myMap
-  myMap.addLayer(markers);
+
+
 });
-
-
-// d3.json(APILink, function(data){
-//   console.log(data);
-// });
-
-
